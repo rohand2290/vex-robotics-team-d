1997870998a97c5615423f91ad578fd8fb458e00
Rohan Deshpande <rohandesh06@gmail.com>
2023-11-24 11:58:21 -0500

    finished some odom work

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include "main.h"
 #include "depend.h"
+#include <Eigen/Dense>
+using Eigen::MatrixXd;
 
 Items items;
 Robot robot;
 
 // Runs initialization code. This occurs as soon as the program is started.
-void initialize() {
+void initialize()
+{
 	pros::lcd::initialize();
 	// initialize objects...
 	items.initialize();
@@ -19,10 +22,10 @@ void disabled() {}
 void competition_initialize() {}
 
 // Runs the user autonomous code.
-void autonomous() {
-	// TODO: 
+void autonomous()
+{
+	// TODO:
 	// while (true) {
-		
 
 	// 	robot.update_coords();
 	// 	pros::delay(AUTON_LOOP_DELAY);
@@ -30,26 +33,32 @@ void autonomous() {
 }
 
 // Runs the operator control code.
-void opcontrol() {
+void opcontrol()
+{
 	pros::lcd::print(1, "starting code...");
 	pros::delay(1000);
 
 	double old_l = 0;
 	double old_r = 0;
 	double old_c = 0;
+	double old_t = 0;
+
+	double new_t = 0;
 
 	double rel_l = 0;
 	double rel_r = 0;
 	double rel_c = 0;
+	double rel_t = 0;
 
 	bool temp = false;
-	// Variables for Smooth Drive:
-	#ifdef SMOOTH_CONSTANT
+// Variables for Smooth Drive:
+#ifdef SMOOTH_CONSTANT
 	int speedr = 0; // speed for right
 	int speedl = 0; // speed for left
-	#endif
+#endif
 	// Driver Code:
-	while (true) {
+	while (true)
+	{
 		rel_l = robot.left_abs_dist() - old_l;
 		rel_r = robot.right_abs_dist() - old_r;
 		rel_c = robot.center_abs_dist() - old_c;
@@ -60,24 +69,27 @@ void opcontrol() {
 			items.master->get_analog(ANALOG_RIGHT_X),
 			__LINE__,
 			speedr,
-			speedl
-		);
+			speedl);
 		// actions acording to buttons:
 		robot.set_intake(items.master->get_digital(DIGITAL_L1));
 		robot.set_turret(
-			items.master->get_digital(DIGITAL_UP), 
-			items.master->get_digital(DIGITAL_DOWN)
-		);
+			items.master->get_digital(DIGITAL_UP),
+			items.master->get_digital(DIGITAL_DOWN));
 		robot.set_puncher(items.master->get_digital(DIGITAL_A));
 		pros::delay(OPCONTROL_LOOP_DELAY);
 
-		if (items.master->get_digital(DIGITAL_Y)) {
-			if (!temp) temp = true;
-			else continue;			
+		if (items.master->get_digital(DIGITAL_Y))
+		{
+			if (!temp)
+				temp = true;
+			else
+				continue;
 			items.initpos = !items.initpos;
 			items.pto1->set_value(items.initpos);
 			items.pto2->set_value(items.initpos);
-		} if (!items.master->get_digital(DIGITAL_Y)) temp = false;
+		}
+		if (!items.master->get_digital(DIGITAL_Y))
+			temp = false;
 		robot.update_coords(rel_l, rel_r, rel_c);
 
 		pros::lcd::print(1, "%f", robot.x);
@@ -87,5 +99,29 @@ void opcontrol() {
 		old_l = robot.left_abs_dist();
 		old_r = robot.right_abs_dist();
 		old_c = robot.center_abs_dist();
+		new_t = calcTheta(old_t, rel_l, rel_r, rel_c);
+	}
+}
+
+double calcTheta(double old_theta, double rel_l, double rel_r, double rel_c)
+{
+	return old_theta + (rel_l - rel_r) / (ROBOT_WIDTH);
+}
+
+MatrixXd local_offset(double new_t, double old_t, double rel_c, double rel_r)
+{
+	if (new_t - old_t == 0)
+	{
+		MatrixXd m(2, 1);
+		m(0, 0) = rel_c;
+		m(0, 1) = rel_r;
+		return m;
+	}
+	else
+	{
+		MatrixXd m(2, 1);
+		m(0, 0) = ((rel_c / (new_t - old_t)) + 9.251969);
+		m(0, 1) = ( (rel_r / (new_t - old_t) + (ROBOT_WIDTH / 2));
+		return m;
 	}
 }
--- src/robot.cpp
+++ src/robot.cpp
@@ -1,15 +1,18 @@
 #include "depend.h"
 #include "robot.h"
 
-double Robot::right_abs_dist() { // in terms of inches
+double Robot::right_abs_dist()
+{ // in terms of inches
     return WHEEL_C * (items.encoder_right->get_position() / 36000);
 }
 
-double Robot::left_abs_dist() { // in terms of inches
+double Robot::left_abs_dist()
+{ // in terms of inches
     return WHEEL_C * (items.encoder_left->get_position() / 36000);
 }
 
-double Robot::center_abs_dist() { // in terms of inches
+double Robot::center_abs_dist()
+{ // in terms of inches
     return WHEEL_C * (items.encoder_center->get_position() / 36000);
 }
 
@@ -30,25 +33,27 @@ void Robot::initialize(Items &i)
 
 Robot::~Robot() {}
 
-void Robot::set_right_side(int analog) {
-    items.left1->move(analog); 
-    items.left2->move(analog); 
+void Robot::set_right_side(int analog)
+{
+    items.left1->move(analog);
+    items.left2->move(analog);
     items.left3->move(analog);
 }
 
-void Robot::set_left_side(int analog) {
-    items.right1->move(analog); 
-    items.right2->move(analog); 
+void Robot::set_left_side(int analog)
+{
+    items.right1->move(analog);
+    items.right2->move(analog);
     items.right3->move(analog);
 }
 
 // here, y represents power and x represents turn, since this is arcade drive style.
-void Robot::set_speed_chassis(int y, int x, long long line, int& speedr, int& speedl)
+void Robot::set_speed_chassis(int y, int x, long long line, int &speedr, int &speedl)
 {
-	int left = (y + (x * TURN_PERCENT)) * MOTOR_PERCENT;
-	int right = (y - (x * TURN_PERCENT)) * MOTOR_PERCENT;
-	// set the wheels...
-	#ifdef SMOOTH_CONSTANT
+    int left = (y + (x * TURN_PERCENT)) * MOTOR_PERCENT;
+    int right = (y - (x * TURN_PERCENT)) * MOTOR_PERCENT;
+// set the wheels...
+#ifdef SMOOTH_CONSTANT
     // if (speedl == nullptr || speedr == nullptr) {
     //     // this should not happen...
     //     pros::lcd::print(1, "ERROR: The chassis speed function has not been setup correctly."
@@ -56,77 +61,98 @@ void Robot::set_speed_chassis(int y, int x, long long line, int& speedr, int& sp
     //     while (true);
     // }
     // left side:
-	if (speedl >= left) speedl -= SMOOTH_CONSTANT;
-	else if (speedl < left) speedl += SMOOTH_CONSTANT;
-	// right side:
-	if (speedr >= right) speedr -= SMOOTH_CONSTANT;
-	else if (speedr < right) speedr += SMOOTH_CONSTANT;
+    if (speedl >= left)
+        speedl -= SMOOTH_CONSTANT;
+    else if (speedl < left)
+        speedl += SMOOTH_CONSTANT;
+    // right side:
+    if (speedr >= right)
+        speedr -= SMOOTH_CONSTANT;
+    else if (speedr < right)
+        speedr += SMOOTH_CONSTANT;
 
-	set_left_side(speedl);
-	set_right_side(speedr);
-	#else
-	set_left_side(left);
-	set_right_side(right);
-	#endif
+    set_left_side(speedl);
+    set_right_side(speedr);
+#else
+    set_left_side(left);
+    set_right_side(right);
+#endif
 }
 
-void Robot::set_intake(int analog)  {// true means in, false means out
-    if (analog) {
+void Robot::set_intake(int analog)
+{ // true means in, false means out
+    if (analog)
+    {
         items.intake_left->move(INTAKE_IN_SPEED);
-		items.intake_right->move(INTAKE_IN_SPEED);
-    } else {
+        items.intake_right->move(INTAKE_IN_SPEED);
+    }
+    else
+    {
         items.intake_left->move(INTAKE_OUT_SPEED);
-		items.intake_right->move(INTAKE_OUT_SPEED);
+        items.intake_right->move(INTAKE_OUT_SPEED);
     }
 }
 
-void Robot::set_turret(int up, int down) {
-    if (up) {
-		items.turret->move(TURRET_SPEED);
-	} else {
-		items.turret->move(0);
-	}
-	if (down) {
-		items.turret->move(-TURRET_SPEED);
-	} else {
-		items.turret->move(0);
-	}
+void Robot::set_turret(int up, int down)
+{
+    if (up)
+    {
+        items.turret->move(TURRET_SPEED);
+    }
+    else
+    {
+        items.turret->move(0);
+    }
+    if (down)
+    {
+        items.turret->move(-TURRET_SPEED);
+    }
+    else
+    {
+        items.turret->move(0);
+    }
 }
 
-void Robot::set_puncher(int analog) {
+void Robot::set_puncher(int analog)
+{
     items.puncher1->set_value(analog);
-	items.puncher2->set_value(analog);
+    items.puncher2->set_value(analog);
 }
 
-double Robot::radians_to_degrees(double radians) {
+double Robot::radians_to_degrees(double radians)
+{
     return (radians * 180) / PI;
 }
 
-double Robot::degrees_to_radians(double degrees) {
+double Robot::degrees_to_radians(double degrees)
+{
     return (degrees * PI) / 180;
 }
 
 // this function is called when an -unusual outside force is detected.
-static void env_push_error() {
+static void env_push_error()
+{
     pros::lcd::print(1, "There was an unwanted push on the bot that has been detected. This is part of the auton routine to ensure everything is working.");
-    while (true);
+    while (true)
+        ;
 }
 
-void Robot::update_coords (double rel_l, double rel_r, double rel_c)
+void Robot::update_coords(double rel_l, double rel_r, double rel_c)
 {
     double r_and_l_speed = items.encoder_right->get_velocity() - items.encoder_left->get_velocity(); // centidegrees per sec
     double c_speed = items.encoder_center->get_velocity();
     // calculate theta:
     /** ============== LOGIC ====================
-    * just r & l: change in theta = (360000 * right speed - left speed)/(pi * width of robot)
-    * just c: change in theta = (360000 * center speed) / (pi * width of robot)
-    * if change with c != change with r & l:
-    *   some outer force pushed the bot.
-    *   do something to accomadate for this.
-    */
+     * just r & l: change in theta = (360000 * right speed - left speed)/(pi * width of robot)
+     * just c: change in theta = (360000 * center speed) / (pi * width of robot)
+     * if change with c != change with r & l:
+     *   some outer force pushed the bot.
+     *   do something to accomadate for this.
+     */
     double by_rl = (360000 * r_and_l_speed) / (PI * ROBOT_WIDTH);
     double by_c = (360000 * c_speed) / (PI * ROBOT_WIDTH);
-    if (by_rl - by_c > ODOM_ACCURACY && by_c - by_rl > ODOM_ACCURACY && CHECK_FOR_ENV_FORCES) {
+    if (by_rl - by_c > ODOM_ACCURACY && by_c - by_rl > ODOM_ACCURACY && CHECK_FOR_ENV_FORCES)
+    {
         env_push_error();
     }
     theta += by_c;
@@ -135,13 +161,13 @@ void Robot::update_coords (double rel_l, double rel_r, double rel_c)
      * t = right speed or left speed depending on which is less
      * vertical translation = sin(theta / 100) * t
      * horizontal translation = cos(theta / 100) * t
-    */
-    double t = items.encoder_right->get_velocity() > items.encoder_left->get_velocity() ? 
-            items.encoder_left->get_velocity() : items.encoder_right->get_velocity();
+     */
+    double t = items.encoder_right->get_velocity() > items.encoder_left->get_velocity() ? items.encoder_left->get_velocity() : items.encoder_right->get_velocity();
     y += sin(theta + 90) * t; // plus 90 since this is an example of bearing applications.
     x += cos(theta + 90) * t;
 }
 
-static double calcTheta(double old_theta, double rel_l, double rel_r, double rel_c) {
+double calcTheta(double old_theta, double rel_l, double rel_r, double rel_c)
+{
     return old_theta + (rel_l - rel_r) / (ROBOT_WIDTH);
 }
